02931d4f120e8970bfab429ba52404520ab86340
Matheus Guilherme <zmatheusguilherme2@gmail.com>
2025-05-19 21:02:57 -0300

    created ✅

---
+++ AdoptPaws/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './index.css' // Tailwind importado aqui
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-start bg-primar">
+      <h1 className="">Pet Adoption 🐶</h1>
+    </div>
+  )
+}
+
+
+export default App
